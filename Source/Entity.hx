2a8df6cb6f8b99988939971162f8156155051a18
Neill Robson <neill@neillrobson.com>
2020-12-16 11:17:44 -0500

    Add resource interface to entity

--- Source/Entity.hx
+++ Source/Entity.hx
@@ -1,5 +1,6 @@
 package;
 
+import Resources.Resource;
 import openfl.geom.Point;
 import openfl.geom.Matrix;
 import openfl.display.Tile;
@@ -49,5 +50,42 @@ class Entity {
         var xd = x - ex;
         var yd = y - ey;
         return (xd * xd + yd * yd) < (r * r + er * er);
+    }
+
+    /**
+        @param searchRadius How large the circle is within which entities are considered.
+        @param centerPointRadius How far away from this entity's origin point can the center of the search circle be.
+    **/
+    public function getRandomTarget(searchRadius:Float, centerPointRadius:Float,
+            filter:TargetFilter):Entity {
+        var xt = x + (Math.random() * 2 - 1) * centerPointRadius;
+        var yt = y + (Math.random() * 2 - 1) * centerPointRadius;
+        return island.getEntityAt(xt, yt, searchRadius, filter);
+    }
+
+    /**
+        Returns the SQUARED distance between two entities. Only to be used for
+        comparison purposes.
+    **/
+    public function distance(e:Entity) {
+        var xd = x - e.x;
+        var yd = y - e.y;
+        return xd * xd + yd * yd;
+    }
+
+    public function givesResource(r:Resource):Bool {
+        return false;
+    }
+
+    public function acceptsResource(r:Resource):Bool {
+        return false;
+    }
+
+    public function gatherResource(r:Resource):Bool {
+        return false;
+    }
+
+    public function submitResource(r:Resource):Bool {
+        return false;
     }
 }
