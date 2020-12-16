c1071d541dc91f8fdac2c6efbbed6dbc0ac0c5f3
Neill Robson <neill@neillrobson.com>
2020-12-16 11:18:26 -0500

    Add resource interface to HouseType

--- Source/HouseType.hx
+++ Source/HouseType.hx
@@ -1,15 +1,18 @@
 package;
 
+import Resources.Resource;
+
 class HouseType {
     private static var id = 0;
 
     public static final houseTypes:Array<HouseType> = new Array();
 
-    public static final MASON = new HouseType(1, "Mason", 10, 8, 12, 0, 15, 0);
-    public static final WOODCUTTER = new HouseType(2, "Woodcutter", 10, 8, 12, 15, 0, 0);
+    public static final MASON = new HouseType(1, "Mason", 10, 8, 12, 0, 15, 0, Resource.ROCK);
+    public static final WOODCUTTER = new HouseType(2, "Woodcutter", 10, 8, 12, 15, 0, 0,
+        Resource.WOOD);
     public static final PLANTER = new HouseType(0, "Planter", 10, 8, 12, 30, 15, 10);
     public static final FARM = new HouseType(6, "Farmer", 10, 8, 12, 30, 30, 0);
-    public static final WINDMILL = new HouseType(7, "Miller", 8, 7, 13, 15, 15, 0);
+    public static final WINDMILL = new HouseType(7, "Miller", 8, 7, 13, 15, 15, 0, Resource.FOOD);
     public static final GUARDPOST = new HouseType(5, "Guardpost", 3, 8, 14, 0, 30, 0);
     public static final BARRACKS = new HouseType(3, "Barracks", 10, 8, 12, 15, 50, 0);
     public static final RESIDENCE = new HouseType(4, "Residence", 10, 8, 12, 30, 30, 30);
@@ -22,8 +25,10 @@ class HouseType {
     public final rock:Int;
     public final food:Int;
     public final name:String;
+    public final acceptResource:Resource;
 
-    private function new(spriteIndex:Int, name:String, radius:Int, anchorX:Int, anchorY:Int, wood:Int, rock:Int, food:Int) {
+    private function new(spriteIndex:Int, name:String, radius:Int, anchorX:Int, anchorY:Int,
+            wood:Int, rock:Int, food:Int, acceptResource:Resource = null) {
         this.spriteIndex = spriteIndex;
         this.radius = radius;
         this.anchorX = anchorX;
@@ -32,6 +37,7 @@ class HouseType {
         this.rock = rock;
         this.food = food;
         this.name = name;
+        this.acceptResource = acceptResource;
         houseTypes[id++] = this;
     }
 
